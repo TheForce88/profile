50bea5e142e1141f07f7f535c181d2d55882531b
eches <Elaine.Chesoni83@gmail.com>
2018-02-11 15:19:35 -0800

    added new branch, modified projects

--- js/projects.js
+++ js/projects.js
@@ -12,11 +12,25 @@ $(document).ready(function() {
   });
 
 // fix toggle so that bar slides out when you hover over "bar" div seperate from nav inside aside
+ $('.sectionhome').hide();
+ $('article').hide();
+
  $('aside').hide().delay(900).fadeIn(2000);
 
 //  $(document).hover(function() {
 //   $( "aside" ).toggle( "slide" );
 // });
 
+  $("aside nav a").each(function() {
+    $(this).on("click", function() {
+    //console.log("working!");
+    var sectionID = $(this).attr("href");
+    console.log(sectionID);
+    $("section, article").hide();
+    $(sectionID).show();
+    $(".sectionhome").show();
+    $('article').show();
+    })
+  });
 
 });
